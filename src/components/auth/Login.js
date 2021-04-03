ab198f7a29f92d4b3f35dfeca3450bd7b648026b
Leonel Vaccaro <fernando.vaccaro@despegar.com>
2021-04-03 16:32:56 -0300

    adding first changes for login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <h1> Estas en el login pei</h1>;
+  // state para iniciar sesion
+  const [usuario, guardarUsuario] = useState({
+    email: '',
+    password: '',
+  });
+
+  // extraer usuario
+
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({ ...usuario, [e.target.name]: e.target.value });
+  };
+
+  // usuario quiere iniciar session
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // validación
+
+    // pasarloa al action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar sesión</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" value={email} placeholder="Tu email" onChange={onChange} />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Igrese su contraseña"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input type="submit" className="btn btn-primario btn-block" value="Iniciar sesión" />
+          </div>
+        </form>
+        <Link to="/nueva-cuenta" className="enlace-cuenta">
+          Obtener cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
